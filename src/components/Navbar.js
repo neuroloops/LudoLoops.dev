08baf83553de76d1b523b1ddf28f6c29a67bf5b4
ludo loops <ludoloops@protonmail.com>
2021-09-17 19:24:41 +0200

    Feat: navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Links from '../constants/links'
+import logo from '../assets/logo_text.svg'
+import email from '../assets/email.svg'
+import github from '../assets/github.svg'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>logo</div>
+    <nav className='navBar'>
+      <img src={logo} alt='LudoLoops logo' className='logoText' />
       <Links />
-      <div>email</div>
+      <div className='email'>
+        <img src={email} alt='email icon' />
+        <span> ludoloops@protonmail.com</span>
+      </div>
+      <div className='github'>
+        <img src={github} alt='github  icon' />
+      </div>
     </nav>
   )
 }
