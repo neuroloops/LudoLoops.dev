82358f1b2cc655f4d5cca04da0029f3311602857
ludo loops <ludoloops@protonmail.com>
2021-10-19 18:19:07 +0200

    feat: responsive with resolution

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
-
-import { isMobile } from 'react-device-detect'
+import { useMediaQuery } from 'react-responsive'
 
 import Layout from '../components/Layout'
 import Navbar from '../components/Navbar'
@@ -8,6 +7,8 @@ import Desktop from '../components/resume/desktop'
 import Mobile from '../components/resume/mobile'
 
 const Resume = () => {
+  const isMobile = useMediaQuery({ query: `(max-width: 1024px)` })
+
   console.log(isMobile)
   return <Layout> {isMobile ? <Mobile /> : <Desktop />} </Layout>
 }
