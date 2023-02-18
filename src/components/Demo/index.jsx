2b70855506053959da583c0ae2c5f9585cc121d5
Ludovic Dumas <ludoloops@pm.me>
2023-02-18 17:56:58 -0500

    add a black underlay bellow the demobox
    commit 5582eddde794ca87d4f0c7d097558ca1db9d17bd
    Author: Ludovic Dumas <ludoloops@pm.me>
    Date:   Sat Feb 18 17:56:47 2023 -0500
    
        finish

--- src/components/Demo/index.jsx
+++ src/components/Demo/index.jsx
@@ -1,69 +1,63 @@
 import React from "react"
-import { css } from "@emotion/react"
+import { style } from "./style"
+
 const Demo = () => {
   return (
     <div
       className="bg--blue"
       id="Demo"
+      css={style.bg}
     >
-      <div className="imgContainer">
-        <img
-          src="/demo/leftBg.png"
-          alt="background image"
-        />
-      </div>
-      <section className="section">
-        <h2>Code Demo</h2>
-        <div css={demoBox}>
-          <iframe
-            height="480"
-            css={codepen}
-            scrolling="no"
-            title="Animated gradient button [pure css]"
-            src="https://codepen.io/ludoloops/embed/NWpZeEw?default-tab=result"
-            frameBorder="no"
-            loading="lazy"
-            allowtransparency="true"
-            allowFullScreen={true}
-          >
-            See the Pen
-            <a href="https://codepen.io/ludoloops/pen/NWpZeEw">
-              Animated gradient button [pure css]
-            </a>
-            by LudoLoops (<a href="https://codepen.io/ludoloops">@ludoloops</a>)
-            on
-            <a href="https://codepen.io">CodePen</a>.
-          </iframe>
+      <div css={style.wrapper}>
+        <div
+        // className="imgContainer"
+        >
+          <img
+            src="/demo/leftBg.png"
+            alt="background image"
+          />
         </div>
-      </section>
-      <div
-        className="imgContainer"
-        css={rightBg}
-      >
-        <img
-          src="/demo/rightBg.png"
-          alt="background image"
-        />
+        <section
+          className="section"
+          css={style.section}
+        >
+          <h2>Code Demo</h2>
+
+          <div css={style.demoBox}>
+            <iframe
+              height="480"
+              css={style.codepen}
+              scrolling="no"
+              title="Animated gradient button [pure css]"
+              src="https://codepen.io/ludoloops/embed/NWpZeEw?default-tab=result"
+              frameBorder="no"
+              loading="lazy"
+              allowtransparency="true"
+              allowFullScreen={true}
+            >
+              See the Pen
+              <a href="https://codepen.io/ludoloops/pen/NWpZeEw">
+                Animated gradient button [pure css]
+              </a>
+              by LudoLoops (
+              <a href="https://codepen.io/ludoloops">@ludoloops</a>) on
+              <a href="https://codepen.io">CodePen</a>.
+            </iframe>
+          </div>
+        </section>
+        <div
+          // className="imgContainer"
+          css={style.rightBg}
+        >
+          <img
+            src="/demo/rightBg.png"
+            alt="background image"
+          />
+        </div>
       </div>
+      <div css={style.bottom}></div>
     </div>
   )
 }
 
 export default Demo
-
-const rightBg = css`
-  align-self: flex-end;
-  margin-right: 62px;
-`
-
-const demoBox = css`
-  height: 566px;
-  width: 656px;
-  background-color: #1f3262;
-  border-radius: 25px;
-`
-
-const codepen = css`
-  width: 100%;
-  background-color: white;
-`
