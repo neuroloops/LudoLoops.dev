9497b8b15bc1bc1a97855a67effa8fe3d9d734a9
Ludovic Dumas <ludoloops@pm.me>
2023-02-27 16:40:09 -0500

    add content for projects section

---
+++ src/components/Projects/Card.jsx
@@ -0,0 +1,68 @@
+import React from "react"
+import { style } from "./style"
+import { css } from "@emotion/react"
+const Card = ({ children, title, img, darker }) => {
+  let bgColor, bgImgColor, noShadow
+
+  if (darker) {
+    bgColor = "background-color: #F4F4F4"
+    bgImgColor = "background-color:#FFFFFF"
+    noShadow = "box-shadow: none"
+  }
+
+  return (
+    <div
+      css={[
+        style.main,
+        css`
+          ${bgColor};
+          ${noShadow}
+        `,
+      ]}
+    >
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+      <div>
+        {img ? (
+          <div
+            css={[
+              style.rightSide,
+              css`
+                ${bgColor};
+              `,
+            ]}
+          >
+            <div
+              css={[
+                style.imgBg,
+                style.imgBgLeft,
+                css`
+                  ${bgImgColor};
+                `,
+              ]}
+            ></div>
+            <div
+              css={[
+                style.imgBg,
+                style.imgBgRight,
+                css`
+                  ${bgImgColor};
+                `,
+              ]}
+            ></div>
+            <div css={style.img}>
+              <img
+                src={"/projects/" + img}
+                alt={title + "image"}
+              />
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,7 +1,8 @@
 import React from "react"
-import { css } from "@emotion/react"
 
-const Project = ({ mdx }) => {
+import Card from "./Card.jsx"
+
+const Project = () => {
   return (
     <div
       className="bg--white"
@@ -9,15 +10,66 @@ const Project = ({ mdx }) => {
     >
       <section className="section">
         <h1>Projects</h1>
-        <div>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla dolore
-          rem mollitia repellat tempore totam numquam, obcaecati eveniet culpa
-          repudiandae accusamus atque iure ut quod molestias. Repellendus
-          aliquid sit perspiciatis.
-        </div>
-        <div>Groupomania</div>
-        <div>link2.md</div>
-        <div>Media center</div>
+
+        <Card
+          title="Incom Delta"
+          img="incomdelta.png"
+        >
+          <p>Making an online store selling Star Wars helmet</p>
+          <p>
+            It is mandatory to make it fast and reliable. I decide to use
+            Netlify for its CDN and its free tier is really welcome for a new
+            business owner
+          </p>
+        </Card>
+        <Card
+          title="Groupomania"
+          img="groupomania.png"
+          darker={true}
+        >
+          <ul>
+            <li>User can create an account</li>
+            <li>user can delete it</li>
+            <li>user can post message and pictures</li>
+            <li>user can edit their message</li>
+          </ul>
+          <ul>
+            <li>admin can delete or edit message</li>
+            <li>admin can add another admin</li>
+            <li>admin can reset user password</li>
+          </ul>
+        </Card>
+        <Card title="link2.md">
+          <p>
+            Link2.md is a Chrome extension that allows you to copy page URL with
+            a description note in markdown format. It is an easy way to create
+            links with text description for any website.
+          </p>
+        </Card>
+        <Card
+          title="Media center"
+          img="mediacenter.png"
+          darker={true}
+        >
+          <p>
+            On this project, I was ask to deploy a streaming system, the
+            customer had to work far away from home and wanted to watch his
+            movie, download file with torrent and synchronize his file on phone
+            and computer. I use docker because it is very light, fast to deploy
+            with easy maintenance.
+          </p>
+          <p>
+            Portainer make it easier for him to manage his server, he is happy
+            to manage it with a GUI instead of CLI.
+          </p>
+        </Card>
+        <Card darker={true}>
+          <h2>Recettes de Moumy</h2>
+        </Card>
+        <Card>
+          <h2>Afgnews</h2>
+          <p>news website, made with nextjs</p>
+        </Card>
       </section>
     </div>
   )
